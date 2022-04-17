650c92f20295258036d18157e6e6a704a52181f0
Abu Jubaer <abujubaer90@gmail.com>
2022-04-18 00:59:13 +0600

    checkout loading problem fixed

--- src/Firebase/RequireAuth.js
+++ src/Firebase/RequireAuth.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { Navigate, useLocation } from 'react-router-dom';
+import CheckOut from '../components/Pages/CheckOut/CheckOut';
+import Loading from '../components/Pages/Loading/Loading';
 import auth from './firebase.init';
 
 const RequireAuth = ({children}) => {
-    const [user]=useAuthState(auth)
+    const [user, loading]=useAuthState(auth)
     const location=useLocation();
+    if(loading){
+        return <Loading></Loading>
+    }
     if (!user) {
         return<Navigate to="/login" state={{from: location} }replace></Navigate>
     }
     return (
         <div>
-
+<CheckOut></CheckOut>
         </div>
     );
 };
--- src/components/Pages/CheckOut/CheckOut.js
+++ src/components/Pages/CheckOut/CheckOut.js
@@ -1,9 +1,11 @@
 import React from 'react';
-
+import { Card } from 'react-bootstrap';
 const CheckOut = () => {
     return (
-        <div>
-            <h2>Thanks for purchasing.</h2>
+        <div className='justify-content-center'>
+            <Card className='m-5 py-5 justify-content-center'>
+             <h1 style={{fontsize: '400px'}} className='text-center text-primary font-xl'>Thanks for taking service.</h1>
+            </Card>
         </div>
     );
 };
